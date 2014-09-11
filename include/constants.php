831995b02b36c2380b532caaae09ff748c50b4c0
ravirohith <ravi.rohith19@gmail.com>
2014-09-10 21:42:42 -0400

    Supermarket management system
    Transactions, Addition of products and editing profiles can be
    performed. Also, automatic email generation when inventory has few items
    left.

---
+++ include/constants.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_SERVER","localhost");
+define("DB_USER","root");
+define("DB_PASSWORD","rap12");
+define("DB_NAME","supermarket");
+//define("FILE_ADD","C:\\xampp\\htdocs\\webblog\\backend\\articles\\");
+?>
